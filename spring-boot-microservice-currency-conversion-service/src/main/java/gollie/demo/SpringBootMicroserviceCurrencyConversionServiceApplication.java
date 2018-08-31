28e4e8a5fe0e24782e79ba575be050e07f953435
Golrokh Moattar <Golrokh.Moattar@skolverket.se>
2018-08-31 15:40:40 +0200

    added currency convertor service

---
+++ spring-boot-microservice-currency-conversion-service/src/main/java/gollie/demo/SpringBootMicroserviceCurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package gollie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("gollie.demo")
+@EnableDiscoveryClient
+public class SpringBootMicroserviceCurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionServiceApplication.class, args);
+	}
+}
