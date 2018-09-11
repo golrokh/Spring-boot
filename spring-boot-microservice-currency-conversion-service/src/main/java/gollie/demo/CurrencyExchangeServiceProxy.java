7bb139486c8bc103eea62cafdbd4692859e45d11
Golrokh Moattar <golrokh.moattar@knowit.se>
2018-09-11 13:21:17 +0200

    Fixed Riboon load balancer

--- spring-boot-microservice-currency-conversion-service/src/main/java/gollie/demo/CurrencyExchangeServiceProxy.java
+++ spring-boot-microservice-currency-conversion-service/src/main/java/gollie/demo/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,12 @@
 package gollie.demo;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversionBean retrieveExchangeValue
